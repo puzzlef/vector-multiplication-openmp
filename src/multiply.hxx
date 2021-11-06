56a65251e70f2fded7e51ab935d510c6c73652e8
Subhajit Sahu <wolfram77@gmail.com>
2021-11-06 11:00:07 +0530

    :skunk: update src

--- src/multiply.hxx
+++ src/multiply.hxx
@@ -1,32 +1,12 @@
 #pragma once
-#include <vector>
-#include <omp.h>
-
-using std::vector;
+#include "_main.hxx"
 
 
 
 
-template <class T>
-void multiply(T *a, const T *x, const T *y, int N) {
-  for (int i=0; i<N; i++)
-    a[i] = x[i] * y[i];
-}
+struct MultiplyOptions {
+  int repeat;
 
-template <class T>
-void multiply(vector<T>& a, const vector<T>& x, const vector<T>& y) {
-  multiply(a.data(), x.data(), y.data(), x.size());
-}
-
-
-template <class T>
-void multiplyOmp(T *a, const T *x, const T *y, int N) {
-  #pragma omp parallel for schedule(static,4096)
-  for (int i=0; i<N; i++)
-    a[i] = x[i] * y[i];
-}
-
-template <class T>
-void multiplyOmp(vector<T>& a, const vector<T>& x, const vector<T>& y) {
-  multiplyOmp(a.data(), x.data(), y.data(), x.size());
-}
+  MultiplyOptions(int repeat=1) :
+  repeat(repeat) {}
+};
